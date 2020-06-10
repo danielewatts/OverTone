d3162c9a08bc8359be58bc47aaf1bfeac21c500b
danielewatts <59425650+danielewatts@users.noreply.github.com>
2020-06-09 18:31:37 -0700

    got bottom navbar hooked up to seperate fragments

--- app/src/main/java/com/example/overtone/MainActivity.java
+++ app/src/main/java/com/example/overtone/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.overtone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeBottomNavigationBar();
+
+
     }
+
+    public void initializeBottomNavigationBar(){
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+    }
+
+
+
+
 }
