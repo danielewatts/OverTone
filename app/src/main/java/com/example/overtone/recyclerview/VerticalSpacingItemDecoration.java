94dc9f9a229b2ab054eb60339f717d154ebaa97b
Daniel Watts <59425650+danielewatts@users.noreply.github.com>
2020-06-20 15:26:01 -0700

    added spacing to chord library item. Need to impliment

---
+++ app/src/main/java/com/example/overtone/recyclerview/VerticalSpacingItemDecoration.java
@@ -0,0 +1,73 @@
+package com.example.overtone.recyclerview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class VerticalSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+    private final int verticalSpaceHeight;
+
+
+    public VerticalSpacingItemDecoration(int verticalSpaceHeight) {
+        this.verticalSpaceHeight = verticalSpaceHeight;
+    }
+
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+        outRect.bottom = verticalSpaceHeight;
+    }
+
+
+    public class DividerItemDecoration extends RecyclerView.ItemDecoration{
+        private final int[] ATTRS = new int[]{android.R.attr.listDivider};
+        private Drawable divider;
+
+        public DividerItemDecoration(Context context){
+            final TypedArray styleAttributes = context.obtainStyledAttributes(ATTRS);
+            divider = styleAttributes.getDrawable(0);
+            styleAttributes.recycle();
+        }
+        public DividerItemDecoration(Context context, int resId){
+            divider = ContextCompat.getDrawable(context,resId);
+        }
+
+        @Override
+        public void onDraw(@NonNull Canvas c, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+            int left = parent.getPaddingLeft();
+            int right = parent.getWidth() - parent.getPaddingRight();
+
+            int childCount = parent.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View child = parent.getChildAt(i);
+
+                RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+                int top = child.getBottom() + params.bottomMargin;
+                int bottom = top + divider.getIntrinsicHeight();
+
+                divider.setBounds(left, top, right, bottom);
+                divider.draw(c);
+            }
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
