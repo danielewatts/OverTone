354301fe9d39114834b153490d0f19554f6651dc
danielewatts <59425650+danielewatts@users.noreply.github.com>
2020-06-23 14:42:45 -0700

    added safeArgs gradle plugin

--- app/src/main/java/com/example/overtone/ChordLibraryFrag.java
+++ app/src/main/java/com/example/overtone/ChordLibraryFrag.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
