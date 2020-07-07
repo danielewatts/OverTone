b503452c4ca39d73594331a38230cfda07e875f3
Daniel Watts <59425650+danielewatts@users.noreply.github.com>
2020-07-07 18:20:54 -0400

    figure out how to dynamically set time waiting ability

--- app/src/main/java/com/example/overtone/PracticeGameFrag.kt
+++ app/src/main/java/com/example/overtone/PracticeGameFrag.kt
@@ -1,19 +1,35 @@
 package com.example.overtone
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_practice_game.*
 
 class PracticeGameFrag : Fragment(),View.OnClickListener{
-
-
     private var chordsInRotation:MutableList<String> = mutableListOf()
     private var bpm:Int? = null
     private var navController:NavController? = null
+    private var mHandler = Handler()
+    private val BPM_MILLISECONDS_CONVERSION =60*1000
+
+    ///this starts the repeating process of selecting a random chord
+    private val mToastRunnable: Runnable = object : Runnable {
+        override fun run() {
+            //// action to be done on a timer
+            Toast.makeText(context, "This is a delayed toast", Toast.LENGTH_SHORT).show()
+            testInfo.text = getRandomChord()
+
+
+            //// Repeatable Action
+            var delay = (1/400)
+            mHandler.postDelayed(this, 4000)
+        }
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -28,7 +44,9 @@ class PracticeGameFrag : Fragment(),View.OnClickListener{
         StopGameBtn.setOnClickListener(this)
         retrievePassedInfo()
         //testing code
-        testInfo.setText(chordsInRotation.toString() +" "+ bpm.toString())
+        ///starts the game
+        startGame()
+        testInfo.text = chordsInRotation.toString() +" "+ bpm.toString()
 
     }
 
@@ -41,14 +59,40 @@ class PracticeGameFrag : Fragment(),View.OnClickListener{
 
 
     }
+    private fun getRandomChord():String{
+        val randomIndex = (0 until chordsInRotation.size).random() // random integer between 0 & size-1
+        return chordsInRotation[randomIndex]
+    }
+
+    private fun startGame() {
+        //mHandler.postDelayed(mToastRunnable, 5000);
+        mToastRunnable.run()
+    }
+
+    private fun stopRepeating() {
+        mHandler.removeCallbacks(mToastRunnable)
+    }
+
 
     override fun onClick(v: View?) {
         when(v!!.id){
-            StopGameBtn.id -> navController?.navigate(R.id.action_practiceGameFrag_to_practiceModeFrag)
+            StopGameBtn.id ->{
+                stopRepeating()
+                navController?.navigate(R.id.action_practiceGameFrag_to_practiceModeFrag)
+
+            }
 
         }
 
     }
 
 
+
+
+
+
+
+
 }
+
+
